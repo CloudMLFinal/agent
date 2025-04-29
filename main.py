7382a82c20b245dc98a170015ee28bae287e4123
kirit <kamiku319@gmail.com>
2025-04-28 22:50:19 -0400

    Add threading support to Kubernetes logging monitor and enhance log streaming

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from kakfa.client import start_consuming
 import logging
+import os
 from dotenv import load_dotenv
+from monitroing.logging import watch_pod_logs
 
 if __name__ == "__main__":
     load_dotenv()
     TOPIC_NAME = "cloudml-logs"
     start_consuming(TOPIC_NAME)
+
+    watch_pod_logs(
+        namespace=os.getenv("MONITORING_NAMESPACE"),
+        label_selector=os.getenv("MONITORING_SELECTOR"),
+    )
